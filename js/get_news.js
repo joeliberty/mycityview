cb2ed311093c8258b8881c4cd63d92b7a8fd2b6f
Joe Liberty <joe@codewest.net>
2015-02-11 18:47:07 +1100

    Clean up.

--- js/get_news.js
+++ js/get_news.js
@@ -1,26 +1,14 @@
 var news_app = angular.module('news_app', ['ui.bootstrap']);
 
-// var locs = {
-//     "Santa_Fe": {"cat":7, "subcat":294}
-// }
-
 news_app.controller("NewsCtrl", function($scope, $rootScope, $http) {
-      //   $scope.selectedItem = '';
-
-      // $scope.SetLocation = function(event) {
-      //   $scope.selectedItem = event;
-      //   console.log('set: ' + $scope.selectedItem)
-      // }
-    // var url = 'http://api.feedzilla.com/v1/categories/547/subcategories/21594/articles.json'
     $scope.isnews = false;
     var locnews = $scope.locnews;
-    var url = 
+    var url = ''
     $http({
         url: url,
         dataType: 'json', 
         method: "GET"
     }).success(function(data) {
-        // console.log(data)
         if(data.articles != '') {
             $scope.localnews = data.articles;
             $scope.isnews = true;
@@ -29,66 +17,59 @@ news_app.controller("NewsCtrl", function($scope, $rootScope, $http) {
             $scope.isnews = false;
             $scope.localnews = {"articles": {"title": "Sorry no local news available.", "summary": ""}};
         }
-        // console.log('localnews: ' + $scope.localnews)
     }); 
 
-    url = 'http://api.feedzilla.com/v1/categories/1071/articles.json'
+    url = ''
     $http({
         url: url,
         dataType: 'json', 
         method: "GET"
     }).success(function(data) {
-        // console.log(data.articles)
         $scope.sportsnews = data.articles;
     }); 
 
-    url = 'http://api.feedzilla.com/v1/categories/1073/articles.json'
+    url = ''
     $http({
         url: url,
         dataType: 'json', 
         method: "GET"
     }).success(function(data) {
-        // console.log(data.articles)
         $scope.travelnews = data.articles;
     }); 
 
-    url = 'http://api.feedzilla.com/v1/categories/1141/articles.json'
+    url = ''
     $http({
         url: url,
         dataType: 'json', 
         method: "GET"
     }).success(function(data) {
-        // console.log(data.articles)
         $scope.topnews = data.articles;
     }); 
 
-    url = 'http://api.feedzilla.com/v1/categories/1082/articles.json'
+    url = ''
     $http({
         url: url,
         dataType: 'json', 
         method: "GET"
     }).success(function(data) {
-        // console.log(data.articles)
         $scope.entertainmentnews = data.articles;
     }); 
 
-    url = 'http://api.feedzilla.com/v1/categories/1086/articles.json'
+    url = ''
     $http({
         url: url,
         dataType: 'json', 
         method: "GET"
     }).success(function(data) {
-        // console.log(data.articles)
         $scope.technews = data.articles;
     }); 
 
-    url = 'http://api.feedzilla.com/v1/categories/1084/articles.json'
+    url = ''
     $http({
         url: url,
         dataType: 'json', 
         method: "GET"
     }).success(function(data) {
-        // console.log(data.articles)
         $scope.musicnews = data.articles;
     }); 
 
@@ -96,7 +77,6 @@ news_app.controller("NewsCtrl", function($scope, $rootScope, $http) {
 
 news_app.filter('removehtml', function () {
   return function (item) {
-    // console.log('fitem: ' + item)
       return item.replace(/<[^>]*>/g, '');
   };
 });
