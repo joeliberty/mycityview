9c617c5a74b7806be662a69515cfbf2041857145
Joe Liberty <joe@codewest.net>
2015-02-11 18:32:43 +1100

    Clean up.

--- js/directives.js
+++ js/directives.js
@@ -36,13 +36,6 @@
       };
     });
 
-    // app.directive("newsmovies", function() {
-    //   return {
-    //     restrict: 'E',
-    //     templateUrl: "partials/newsmovies.html"
-    //   };
-    // });
-
     app.directive("news", function() {
       return {
         restrict: 'E',
@@ -71,31 +64,10 @@
       };
     });
 
-    // app.directive("productSpecs", function() {
-    //   return {
-    //     restrict:"A",
-    //     templateUrl: "product-specs.html"
-    //   };
-    // });
-
     app.directive("headerImage", function() {
       return {
         restrict: "E",
         templateUrl: "partials/header-image.html",
       };
     });
-
-    // app.directive("productGallery", function() {
-    //   return {
-    //     restrict: "E",
-    //     templateUrl: "product-gallery.html",
-    //     controller: function() {
-    //       this.current = 0;
-    //       this.setCurrent = function(imageNumber){
-    //         this.current = imageNumber || 0;
-    //       };
-    //     },
-    //     controllerAs: "gallery"
-    //   };
-    // });
-  })();
+})();
