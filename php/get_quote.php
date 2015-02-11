3064e57d9ef545a00accab0b8e6c43d204ec63b0
Joe Liberty <joe@codewest.net>
2015-02-11 18:54:06 +1100

    Clean up.

--- php/get_quote.php
+++ php/get_quote.php
@@ -3,7 +3,7 @@
 require_once 'unirest-php-master/lib/Unirest.php';
 $response = Unirest::post("https://andruxnet-random-famous-quotes.p.mashape.com/cat=famous",
   array(
-    "X-Mashape-Key" => "2ZSI72udG6mshBsov6QKtZmPZrscp1JrCYljsnmqaqmCWkpxiT",
+    "X-Mashape-Key" => "",
     "Content-Type" => "application/x-www-form-urlencoded"
   )
 );
