449d13e70057824bb39a1d79456ddef93a3618e7
Joe Liberty <joe@codewest.net>
2015-02-11 18:41:00 +1100

    Clean up.

--- js/get_horoscope.js
+++ js/get_horoscope.js
@@ -11,7 +11,7 @@ astro_app.controller("AstroCtrl", function($scope, $http) {
     $scope.horoscope = '';
     $('#astro_horoscope i').css('display', 'inline-block');
     $http({
-      url: , 
+      url: '', 
       method: "POST",
       dataType: 'json',
       data: JSON.stringify({ sign: sign})
@@ -21,33 +21,7 @@ astro_app.controller("AstroCtrl", function($scope, $http) {
       // console.log('data: ' + data)
       $scope.horoscope = data.horoscope;
     }); 
-
-    // $http({
-    //   url: 'http://localhost/bootstrap/get_astro.php', 
-    //   method: "GET",
-    //   params: {sign: sign}
-    // })
-    // .success(function(data) {
-    //   var data_array = jQuery.parseJSON(data);
-    //   var data_array = eval('(' + data_array + ')');
-    //   var type = Object.prototype.toString.call(data_array);
-    //   $('#astro_forcast i').css('display', 'none');
-    //   $scope.forcast = data_array.horoscope.horoscope;
-    //   // console.log('forcast: ' + $scope.forcast)
-    // }); 
-
-    // $.ajax({
-    //   url: "https://sender.blockspring.com/api_v2/blocks/dba3c2ca01c063df9cdf9fc6f0cf93f9?api_key=db81b1fa591380eb4110ff3093829176",
-    //   type: "POST",
-    //   contentType: "application/json",
-    //   data: JSON.stringify({ sign: sign}),
-    //   crossDomain: true
-    // }).done(function(response){
-    //   $('#astro_forcast').text(response);
-    //   console.log(response);
-    // });
   };
-
 });
 
 
